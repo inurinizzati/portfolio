d173e09dbb6609c5939f7fcbf6aef61bba72ea96
Nurin Izzati <117788773+inurinizzati@users.noreply.github.com>
2025-01-07 14:12:56 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Hero from './Hero';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
 
 function App() {
   return (
